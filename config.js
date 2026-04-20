4fb3920af06b9fecf8f24e3c803fb1bbdc3c18c6
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-21 00:02:21 +0530

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "uBRzzTjI#MM1OBDr7Dq9YM6yeyY0B9Swril09gcXL94Iucxi2vQc",
+SESSION_ID: process.env.SESSION_ID || "rYYhlKgS#0l5W0Sgfy8vqQhuojC9m26VTF3hwpS4PqRaO4sl0y0U",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
 AUTO_REPLY: process.env.AUTO_REPLY || "true",
