f96ea1d5b9074f84e3240646c9beced77ae32800
HASHU-MD <dinujavirajitha364@gmail.com>
2026-04-21 00:23:30 +0530

    Update configuration defaults for session and features

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "rYYhlKgS#0l5W0Sgfy8vqQhuojC9m26VTF3hwpS4PqRaO4sl0y0U",
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
-ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "true",
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
+SESSION_ID: process.env.SESSION_ID || "7URRVbZD#tdwJZjw2uv2CIFe5UcUiAADhEt0YCwn5lSLOa4jPDP8",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+ANTI_BAD_WORD: process.env.ANTI_BAD_WORD || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/l06cb8.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HELLO IM YOURE NAME CREATED BY YOUR NAME <NOW ALIVE> "
 };
